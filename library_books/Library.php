5c9a062e38b0994ee7039c243aceb2d30dfd5c99
Timur <timurcool650@gmail.com>
2023-04-03 02:34:44 +0600

    Abstract Library

--- library_books/Library.php
+++ library_books/Library.php
@@ -1,6 +1,33 @@
 <?php
+require_once AbstractLibrary::class;
+require_once Author::class;
 
 class Library extends AbstractLibrary
 {
+    public function addAuthor(string $authorName): Author
+    {
+        $author = new Author($authorName);
+        $this->authors[] = $author;
+        return $author;
+    }
 
+    public function addBookForAuthor($authorName, Book $book)
+    {
+        // TODO: Implement addBookForAuthor() method.
+    }
+
+    public function getBooksForAuthor($authorName)
+    {
+        // TODO: Implement getBooksForAuthor() method.
+    }
+
+    public function search(string $bookName): ?Book
+    {
+        // TODO: Implement search() method.
+    }
+
+    public function print()
+    {
+        // TODO: Implement print() method.
+    }
 }
